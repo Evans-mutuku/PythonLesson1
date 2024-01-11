a8b3235c8d54218bcb802600b753326459f17cd7
Evans-mutuku <evansnyamai98@gmail.com>
2024-01-11 19:55:43 +0300

    my 300 days of code

--- OOP1/classes.py
+++ OOP1/classes.py
@@ -1,1 +1,17 @@
-print("Hello world")
+# Python Classes
+
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    def greet(self):
+        return f"Hello, my name is {self.name} and my age is {self.age} my gender is {self.gender}"
+
+
+person1 = Person("John", 22, "Male")
+
+print(person1.greet())
+
+
+# person = person.Person("sdknsd", 34)
